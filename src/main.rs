927de6394c09caad60099be7d924c9ff06e9ee0e
liminallogs@proton.me <liminallogs@proton.me>
2025-12-16 19:49:14 -0500

    added lib.rs, moved logic from main there, created test folder with health check, lib/bin in manifest

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
-
-async fn health_check(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
-
+use z2p::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    run().await
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+        assert!(response.status().is_success())
+    }
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,1 @@
+use z2p::main;
